d97b9a8ae3cb073036a7daebef2b0b5820c9b5b2
truongdinhduyhai1010 <87969695+truongdinhduyhai1010@users.noreply.github.com>
2024-09-12 10:13:41 +0700

    Create cointants.js

---
+++ cointants.js
@@ -0,0 +1,6 @@
+const VERIFY_TASK_MAPPING = {
+  "38f6dd88-57bd-4b42-8712-286a06dac0a0": "VALUE",
+  "6af85c01-f68d-4311-b78a-9cf33ba5b151": "GO GET",
+};
+
+module.exports = { VERIFY_TASK_MAPPING };
